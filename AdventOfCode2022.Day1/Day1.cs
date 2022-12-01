626c78118e537de31a4847936b042322d55365ba
shanekeaneppb <113368721+shanekeaneppb@users.noreply.github.com>
2022-12-01 15:10:16 +0000

    Completed Day 1

---
+++ AdventOfCode2022.Day1/Day1.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode2022.Day1
+{
+    public class Day1Solutions
+    {
+
+        public static void Part1()
+        {
+            Console.WriteLine($"Day 1, Part 1 Solution:");
+            Console.WriteLine($"Max Calories = {MaxNCalories(1)}");
+        }
+        public static void Part2()
+        {
+            Console.WriteLine($"Day 1, Part 1 Solution:");
+            Console.WriteLine($"Max 3 Calories = {MaxNCalories(3)}");
+        }
+
+        private static int MaxNCalories(int n)
+        {
+            string textFile = @"../../../../AdventOfCode2022.Day1/input.txt";
+            int currentElfCalories = 0;
+            List<int> caloriesList = new List<int>();
+            string[] caloriesLines = File.ReadAllLines(textFile);
+
+            foreach (string calories in caloriesLines)
+            {
+                if (calories == "")
+                {
+                    caloriesList.Add(currentElfCalories);
+                    currentElfCalories = 0;
+                }
+                else
+                {
+                    currentElfCalories += Convert.ToInt32(calories);
+                }
+            }
+
+            int maxNCalories = (from item in caloriesList
+                                orderby item descending
+                                select item
+                                ).Take(n).Sum();
+
+            return maxNCalories;
+        }
+    }
+
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace AdventOfCode2022
+﻿using AdventOfCode2022.Day1;
+
+namespace AdventOfCode2022
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            // Day 1
+            Day1Solutions.Part1();
+            Console.WriteLine();
+            Day1Solutions.Part2();
         }
     }
   
