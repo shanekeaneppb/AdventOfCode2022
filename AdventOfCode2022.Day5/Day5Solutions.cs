a842f737c20fb377f93855a7beb0e60954787a41
shanekeaneppb <113368721+shanekeaneppb@users.noreply.github.com>
2022-12-05 10:06:39 +0000

    Day 5 setup

---
+++ AdventOfCode2022.Day5/Day5Solutions.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2022.Day5
+{
+    public class Day5Solutions
+    {
+        public static void Part1()
+        {
+
+        }
+        public static void Part2()
+        {
+
+        }
+    }
+}
