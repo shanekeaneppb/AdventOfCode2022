222ecf2df0230bb2e509420479834f6cf581e563
shanekeaneppb <113368721+shanekeaneppb@users.noreply.github.com>
2022-12-06 10:24:09 +0000

    Completed Day6

---
+++ AdventOfCode2022.Day6/Day6Solutions.cs
@@ -0,0 +1,44 @@
+﻿namespace AdventOfCode2022.Day6
+{
+    public class Day6Solutions
+    {
+        public static void Part1()
+        {
+            int i = GetStartingIndex(4);
+            Console.Write($"Day 6, Part 1 Solution: {i}");
+        }
+        public static void Part2()
+        {
+            int i = GetStartingIndex(14);
+            Console.Write($"Day 6, Part 2 Solution: {i}");
+        }
+
+        private static int GetStartingIndex(int markerSize)
+        {
+            int i = 0;
+            using (StreamReader file = new StreamReader(@"../../../../AdventOfCode2022.Day6/input.txt"))
+            {
+                string ln;
+                Queue<char> marker = new Queue<char>();
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    i = 1;
+                    foreach (char c in ln)
+                    {
+                        marker.Enqueue(c);
+
+                        if (i >= markerSize)
+                        {
+                            if (marker.Distinct().Count() == marker.Count())
+                                break;
+                            marker.Dequeue();
+                        }
+                        i++;
+                    }
+                }
+            }
+            return i;
+        }
+    }
+}
