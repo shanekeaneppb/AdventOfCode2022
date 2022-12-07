2993062fd115b2472e81929d9413d332861fcec7
shanekeaneppb <113368721+shanekeaneppb@users.noreply.github.com>
2022-12-07 15:30:32 +0000

    Day 7 setup

---
+++ AdventOfCode2022.Day7/Day7Solutions.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2022.Day7
+{
+    public class Day7Solutions
+    {
+        public static void Part1()
+        {
+            Console.Write($"Day 6, Part 1 Solution: ");
+        }
+        public static void Part2()
+        {
+            Console.Write($"Day 6, Part 2 Solution: ");
+        }
+    }
+}
