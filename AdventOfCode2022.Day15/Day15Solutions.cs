7f31dd593b206770ce12db81fe6c5ca3880761e0
shanekeaneppb <113368721+shanekeaneppb@users.noreply.github.com>
2022-12-17 13:32:45 +0000

    Day 15 setup

---
+++ AdventOfCode2022.Day15/Day15Solutions.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2022.Day15
+{
+    public class Day15Solutions
+    {
+        public static void Part1()
+        {
+            Console.WriteLine($"Day 15, Part 1 Solution: ");
+        }
+        public static void Part2()
+        {
+            Console.WriteLine($"Day 15, Part 1 Solution: ");
+        }
+    }
+}
