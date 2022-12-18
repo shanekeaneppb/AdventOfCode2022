b25e4eec20824b9a7329a00461931671a2fdb381
shanekeaneppb <113368721+shanekeaneppb@users.noreply.github.com>
2022-12-18 14:45:00 +0000

    Day17 setup

---
+++ AdventOfCode2022.Day17/Day17Solutions.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2022.Day17
+{
+    public class Day17Solutions
+    {
+        public static void Part1()
+        {
+            Console.WriteLine($"Day 17, Part 1 Solution: ");
+        }
+        public static void Part2()
+        {
+            Console.WriteLine($"Day 17, Part 2 Solution: ");
+        }
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -11,6 +11,7 @@ using AdventOfCode2022.Day10;
 using AdventOfCode2022.Day11;
 using AdventOfCode2022.Day12;
 using AdventOfCode2022.Day14;
+using AdventOfCode2022.Day17;
 
 namespace AdventOfCode2022
 {
@@ -63,13 +64,17 @@ namespace AdventOfCode2022
             //Console.WriteLine();
             //Day11Solutions.Part2();
 
-            Day12Solutions.Part1();
-            Console.WriteLine();
-            Day12Solutions.Part2();
+            //Day12Solutions.Part1();
+            //Console.WriteLine();
+            //Day12Solutions.Part2();
 
             //Day14Solutions.Part1();
             //Console.WriteLine();
             //Day14Solutions.Part2();
+
+            Day17Solutions.Part1();
+            Console.WriteLine();
+            Day17Solutions.Part2();
         }
     }
   
