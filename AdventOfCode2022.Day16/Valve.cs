904d9906839a622664de031ce2faf8618d62aec0
shanekeaneppb <113368721+shanekeaneppb@users.noreply.github.com>
2022-12-20 12:54:23 +0000

    no change

---
+++ AdventOfCode2022.Day16/Valve.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day16
+{
+    public class Valve
+    {
+        public string Name = null;
+        public int FlowRate;
+        public List<Valve> Neighbours;
+
+        public Valve()
+        {
+            Neighbours = new();
+        }
+
+        public Valve(string name, int flowRate)
+        {
+            Name = name;
+            FlowRate = flowRate;
+            Neighbours = new();
+        }
+
+        public void AddNeighbours(string[] neighbourNames, Dictionary<string, Valve> existingValves)
+        {
+            string neighbour;
+            foreach(string n in neighbourNames)
+            {
+                neighbour = n.Trim();
+                if(existingValves.ContainsKey(neighbour))
+                    this.Neighbours.Add(existingValves[neighbour]);
+                else
+                {
+                    existingValves.Add(neighbour, new Valve());
+                    this.Neighbours.Add(existingValves[neighbour]);
+                }
+            }
+        }
+    }
+
+    
+}
---
+++ AdventOfCode2022.Day16/ValveInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode2022.Day16
+{
+    public partial class Day16Solutions
+    {
+        public class ValveInfo
+        {
+            public int Distance;
+            public int FlowRate;
+
+            public ValveInfo()
+            { }
+            public ValveInfo(int distance, int flowRate)
+            {
+                Distance = distance;
+                FlowRate = flowRate;
+            }
+        }
+    }
+}
