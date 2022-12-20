594ceee3bdc1dd97ee2747e2fa9d65c58fc9a152
shanekeaneppb <113368721+shanekeaneppb@users.noreply.github.com>
2022-12-20 11:17:02 +0000

    Was misreading the question

---
+++ AdventOfCode2022.Day15/Beacon.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AdventOfCode2022.Day15
+{
+    public class Beacon
+    {
+        public int X;
+        public int Y;
+
+        public Beacon(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString() => $"({X}, {Y})";
+    }
+}
---
+++ AdventOfCode2022.Day15/Sensor.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode2022.Day15
+{
+    public class Sensor
+    {
+        public int X;
+        public int Y;
+        public Beacon beacon;
+        public int DistToBeacon;
+
+        public Sensor(int x, int y, Beacon beacon)
+        {
+            X = x;
+            Y = y;
+            this.beacon = beacon;
+            DistToBeacon = DistanceToBeacon(this.beacon);
+
+        }
+        public int DistanceToBeacon(Beacon beacon)
+        {
+            return Math.Abs(this.X - beacon.X) + Math.Abs(this.Y - beacon.Y);
+        }
+        public override string ToString() => $"({X}, {Y})";
+    }
+}
