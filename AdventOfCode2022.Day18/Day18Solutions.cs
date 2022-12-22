bd36d30df0b9c805f5c79742c0d46562cb095f0c
shanekeaneppb <113368721+shanekeaneppb@users.noreply.github.com>
2022-12-22 11:13:42 +0000

    Day18 setup

---
+++ AdventOfCode2022.Day18/Day18Solutions.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode2022.Day18
+{
+    public class Day18Solutions
+    {
+
+        public struct Cube
+        {
+            public int x;
+            public int y;
+            public int z;
+
+            //public Cube(string coord)
+            //{
+
+            //}
+        }
+        public static void Part1()
+        {
+            Console.WriteLine($"Day 18, Part 1 Solution:");
+        }
+        public static void Part2()
+        {
+            Console.WriteLine($"Day 18, Part 2 Solution:");
+        }
+
+    }
+}
