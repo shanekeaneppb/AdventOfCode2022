ba8ae81a97d6ed0f4a41234dc5a505fc8d9d14d1
shanekeaneppb <113368721+shanekeaneppb@users.noreply.github.com>
2022-12-23 15:18:35 +0000

    Not moving elements correctly

--- AdventOfCode2022.Day20/Day20Solutions.cs
+++ AdventOfCode2022.Day20/Day20Solutions.cs
@@ -7,103 +7,56 @@ namespace AdventOfCode2022.Day20
         public static void Part1()
         {
             int[] indices = new int[] { 1000, 2000, 3000 };
-            List<int> list = LoadFile("test.txt");
+            (MyLinkedList list, List<int> originalOrder) = LoadFile("test.txt");
             // 3859 is incorrect. Answer is too high
             // 1769 is incorrect. Answer is too low
-            int[] originalOrder = list.ToArray();
-
+            
             DecryptMessage(list, originalOrder);
+            Console.WriteLine(list);
+            //int groveCoordinatesSum = GetGroveCoordinatesSum(list, indices);
 
-            int groveCoordinatesSum = GetGroveCoordinatesSum(list, indices);
-
-            Console.WriteLine($"Day 20, Part 1 Solution: {groveCoordinatesSum}");
+            //Console.WriteLine($"Day 20, Part 1 Solution: {groveCoordinatesSum}");
         }
         public static void Part2()
         {
             Console.WriteLine($"Day 20, Part 2 Solution: ");
         }
 
-        private static List<int> LoadFile(string file)
+        private static (MyLinkedList, List<int>) LoadFile(string file)
         {
             List<int> list;
+            MyLinkedList myList = new();
             using (StreamReader reader = new StreamReader(@"../../../../AdventOfCode2022.Day20/" + file))
             {
                 list = reader.ReadToEnd().Trim().Split("\r\n").Select(p => Convert.ToInt32(p)).ToList();
             }
-            return list;
+            foreach (int i in list)
+            {
+                myList.Add(i);
+            }
+            return (myList, list);
         }
 
-        private static void DecryptMessage(List<int> list, int[] originalOrder)
+        private static void DecryptMessage(MyLinkedList list, List<int> originalOrder)
         {
-            int length = originalOrder.Length;
-            int currentPosition;
+            int length = list.Length;
             int currentNumber;
-            int newPosition;
-            int multiplier;
-            for(int i = 0; i < length; i++)
+            int stepsToMove;
+            //Console.WriteLine(list);
+            for (int i = 0; i < length; i++)
             {
                 currentNumber = originalOrder[i];
-                currentPosition = list.IndexOf(currentNumber);
-                multiplier = Math.Abs(currentNumber/length);
-                newPosition = ((currentPosition + currentNumber) + (multiplier + 1)*length) % length;
-
-                // Moving right
-                if (currentNumber > 0)
-                {
-                    if (newPosition == length - 1)
-                    {
-                        list.Insert(0, currentNumber);
-                        list.RemoveAt(currentPosition + 1);
-                    }
-                    else if (newPosition == 0)
-                    {
-                        list.Insert(1, currentNumber);
-                        list.RemoveAt(currentPosition + 1);
-                    }
-                    else if (newPosition > currentPosition)
-                    {
-                        list.Insert(newPosition + 1, currentNumber);
-                        list.RemoveAt(currentPosition);
-                    }
-                    else if (newPosition < currentPosition)
-                    {
-                        list.Insert(newPosition + 1, currentNumber);
-                        list.RemoveAt(currentPosition + 1);
-                    }
-                }
-
-                // Moving left
-                if (currentNumber < 0)
-                {
-                    if (newPosition == length - 1)
-                    {
-                        list.Insert(length - 1, currentNumber);
-                        list.RemoveAt(currentPosition);
-                    }
-                    else if (newPosition == 0)
-                    {
-                        list.Add(currentNumber);
-                        list.RemoveAt(currentPosition);
-                    }
-                    else if (newPosition > currentPosition)
-                    {
-                        list.Insert(newPosition, currentNumber);
-                        list.RemoveAt(currentPosition);
-                    }
-                    else if (newPosition < currentPosition)
-                    {
-                        list.Insert(newPosition, currentNumber);
-                        list.RemoveAt(currentPosition + 1);
-                    }
-                }
+                list.Move(currentNumber);
+                //Console.WriteLine(list);
+                //Console.WriteLine();
             }
         }
         private static int GetGroveCoordinatesSum(List<int> list, int[] indices)
         {
             int zeroIndex = list.IndexOf(0);
             int groveCoordinatesSum = 0;
-            foreach(int index in indices)
-                groveCoordinatesSum += list[(zeroIndex + index)%list.Count];
+            foreach (int index in indices)
+                groveCoordinatesSum += list[(zeroIndex + index) % list.Count];
             return groveCoordinatesSum;
         }
     }
---
+++ AdventOfCode2022.Day20/MyLinkedList.cs
@@ -0,0 +1,111 @@
+﻿namespace AdventOfCode2022.Day20
+{
+    public class MyLinkedList
+    {
+        public class Node
+        {
+            public int Value;
+            public Node Previous;
+            public Node Next;
+
+            public Node(int value)
+            {
+                Value = value;
+            }
+
+            //public override string ToString()
+            //{
+            //    return $"{Value}, Next = {Next}, Previous = {Previous}";
+            //}
+        }
+
+        public Node Start;
+        public Node End;
+        public int Length;
+
+        public void Add(int value)
+        {
+            Node newNode = new Node(value);
+            if (Length == 0)
+            {
+                newNode.Previous = newNode;
+                newNode.Next = newNode;
+                Start = newNode;
+                End = newNode;
+            }
+            else
+            {
+                // initialise new node at end
+                newNode.Previous = End;
+                newNode.Next = Start;
+
+                // Maintain the loop
+                End.Next = newNode;
+                Start.Previous = newNode;
+
+                // Set the new end node
+                End = newNode;
+            }
+            Length++;
+        }
+
+        public void Move(int numberToMove)
+        {
+            //Console.WriteLine($"Current list = {this}");
+            int stepsToMove = numberToMove % (Length - 1);
+            int i, index;
+            Node nodeToMove = Start, newNodePosition;
+            for(i = 0; i < Length; i++)
+            {
+                if (nodeToMove.Value == numberToMove)
+                    break;
+                nodeToMove = nodeToMove.Next;
+            }
+            newNodePosition = nodeToMove;
+
+            if (stepsToMove == 0)
+                return;
+            if(numberToMove < 0)
+            {
+                for(i = 0; i < Math.Abs(stepsToMove); i++)
+                {
+                    newNodePosition = newNodePosition.Previous;
+                }
+            }
+            else if (numberToMove > 0)
+            {
+                for (i = 0; i < Math.Abs(stepsToMove); i++)
+                {
+                    newNodePosition = newNodePosition.Next;
+                }
+            }
+            // Remove the node that we are moving
+            nodeToMove.Previous.Next = nodeToMove.Next;
+            nodeToMove.Next.Previous = nodeToMove.Previous;
+            //Console.WriteLine($"After Removing = {this}");
+
+            // Insert the new node
+            nodeToMove.Previous = newNodePosition;
+            nodeToMove.Next = newNodePosition.Next;
+
+            newNodePosition.Next.Previous = nodeToMove;
+            newNodePosition.Next = nodeToMove;
+
+        }
+
+
+        public override string ToString()
+        {
+            string str = "";
+            Node node = Start;
+            for(int i = 0; i < Length; i++)
+            {
+                str += $"{node.Value}, ";
+                node = node.Next;
+            }
+            return str;
+        }
+    }
+}
+
+
